b1ed7659a845b0f00f9fc57f98cc085f57f603f9
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2021-11-20 20:28:26 +0100

    Exercise 7

---
+++ 2_variables_and_simple_data_types/7_strip.py
@@ -0,0 +1,16 @@
+# Stripping whitespace
+
+favorite_language = "Python "
+print(favorite_language)
+
+# Remove TEMPORARILY whitespace at the right end of a string
+favorite_language.rstrip()
+
+# Remove PERMANENTLY whitespace at the right end of a string
+favorite_language = favorite_language.rstrip()
+
+# Remove TEMPORARILY whitespace at the left end of a string
+favorite_language.lstrip()
+
+# Remove TEMPORARILY whitespace at both ends of a string
+favorite_language.strip()
